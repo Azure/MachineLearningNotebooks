73c5d02880fbce0837b1397194c9f4e51936d7b4
Sheri Gilley <sgilley@microsoft.com>
2018-12-17 12:23:03 -0600

    Update quickstart.py

--- ignore/doc-qa/quickstart-create-workspace-with-python/quickstart.py
+++ ignore/doc-qa/quickstart-create-workspace-with-python/quickstart.py
@@ -7,7 +7,7 @@ print(azureml.core.VERSION)
 # this is NOT a snippet.  If this code changes, go fix it in the article!
 from azureml.core import Workspace
 ws = Workspace.create(name='myworkspace',
-                      subscription_id='<subscription-id',
+                      subscription_id='<subscription-id>',
                       resource_group='myresourcegroup',
                       create_resource_group=True,
                       location='eastus2' # or other supported Azure region
