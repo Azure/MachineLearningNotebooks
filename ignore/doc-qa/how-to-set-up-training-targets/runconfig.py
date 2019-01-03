cc688caa4e250423c65617193490f3e5dd14ec4c
Sheri Gilley <sgilley@microsoft.com>
2019-01-03 08:53:49 -0600

    change names

--- ignore/doc-qa/how-to-set-up-training-targets/runconfig.py
+++ ignore/doc-qa/how-to-set-up-training-targets/runconfig.py
@@ -1,4 +1,4 @@
-# <system-managed>
+# <systemManaged>
 from azureml.core.runconfig import RunConfiguration
 from azureml.core.conda_dependencies import CondaDependencies
 
@@ -6,11 +6,11 @@ run_system_managed = RunConfiguration()
 
 # Specify the conda dependencies with scikit-learn
 run_system_managed.environment.python.conda_dependencies = CondaDependencies.create(conda_packages=['scikit-learn'])
-# </system-managed>
+# </systemManaged>
 print(run_system_managed)
 
 
-# <user-managed>
+# <user_managed>
 from azureml.core.runconfig import RunConfiguration
 
 run_user_managed = RunConfiguration()
@@ -18,6 +18,6 @@ run_user_managed.environment.python.user_managed_dependencies = True
 
 # Choose a specific Python environment by pointing to a Python path. For example: 
 # run_config.environment.python.interpreter_path = '/home/ninghai/miniconda3/envs/sdk2/bin/python'
-# </user-managed>
+# </user_managed>
 print(run_user_managed)
 
