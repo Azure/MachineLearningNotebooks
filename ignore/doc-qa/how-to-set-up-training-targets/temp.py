fb760f648d3513a12385765843dab62681f27493
Sheri Gilley <sgilley@microsoft.com>
2019-01-07 12:58:32 -0600

    Delete temp.py

--- ignore/doc-qa/how-to-set-up-training-targets/temp.py
+++
@@ -1,8 +0,0 @@
-from azureml.core import Workspace
-ws = Workspace.from_config()
-
-#<amlcompute_temp>
-from azureml.core.compute import ComputeTarget, AmlCompute
-
-# First, list the supported VM families for Azure Machine Learning Compute
-print(AmlCompute.supported_vmsizes(workspace=ws))
