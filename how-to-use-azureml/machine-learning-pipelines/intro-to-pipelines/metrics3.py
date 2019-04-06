4968f8ddb6a873316fffa23e1e936273afda55cc
Anders Swanson <anders.swanson@avanade.com>
2019-04-05 19:11:57 -0700

    updates to method

--- how-to-use-azureml/machine-learning-pipelines/intro-to-pipelines/metrics3.py
+++ how-to-use-azureml/machine-learning-pipelines/intro-to-pipelines/metrics3.py
@@ -1,9 +1,9 @@
 import argparse
 import os
-import pprint
+from pprint import pprint
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--input_file', dest="input_dir", default = "C://temp/attrition_pipe/output")
+parser.add_argument('--input_file', dest="input_file", default = "C://temp/attrition_pipe/output")
 parser.add_argument('--output_dir', dest="output_dir", default = "C://temp/attrition_pipe/output")
 
 args = parser.parse_args()
@@ -12,14 +12,13 @@ print("all args: ", args)
 cwd = os.getcwd()
 print("cwd:", cwd)
 print("dir of cwd", os.listdir(cwd))
-parent = os.path.dirname(args.input_dir)
+parent = os.path.dirname(args.input_file)
 print("input_dir_parent:", parent)
 print("dir of input_dir_parent:", os.listdir( parent))
 
-print("cwd:", args.input_dir)
-print("dir of cwd", os.listdir( args.input_dir))
+print("file path:", args.input_file)
 
-with open(os.path.join(args.input_dir)) as f:
+with open(os.path.join(args.input_file)) as f:
     metrics = json.load(f)
 
-pprint(metrics, wdith = 1)
+pprint(metrics, width = 1)
