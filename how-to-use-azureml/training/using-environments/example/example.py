e792ba827839b2e0670b1411a7c329d46a404e67
Vitaliy Zhurba <vizhur@microsoft.com>
2019-07-12 10:27:43 -0400

    MLN repo autocleanup

--- how-to-use-azureml/training/using-environments/example/example.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License
-
-# Very simple script to demonstrate run in environment
-# Print message passed in as environment variable
-import os
-
-print(os.environ.get("MESSAGE"))
