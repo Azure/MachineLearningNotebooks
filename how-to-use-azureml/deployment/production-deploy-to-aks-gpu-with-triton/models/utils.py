79d97e47cde7514411b88e375ef509f7894c64c4
Yifei Yu <yifyu@microsoft.com>
2020-08-17 22:40:33 +0800

    refinements

--- how-to-use-azureml/deployment/production-deploy-to-aks-gpu-with-triton/models/utils.py
+++ how-to-use-azureml/deployment/production-deploy-to-aks-gpu-with-triton/models/utils.py
@@ -1,21 +1,9 @@
-#!/usr/bin/env python
-
-import argparse
 import numpy as np
 from PIL import Image
-import sys
-from functools import partial
-import os
-import io
-
-import tritonhttpclient
 from tritonclientutils import triton_to_np_dtype
-from tritonclientutils import InferenceServerException
 
-from azureml.contrib.services.aml_request import AMLRequest, rawhttp
-from azureml.contrib.services.aml_response import AMLResponse
 
-def preprocess(img, scaling):
+def preprocess(img, scaling, dtype):
     """
     Pre-process an image to meet the size, type and format
     requirements specified by the parameters.
@@ -25,7 +13,6 @@ def preprocess(img, scaling):
     h = 224
     w = 224
     format = "FORMAT_NCHW"
-
     
     if c == 1:
         sample_img = img.convert('L')
@@ -85,43 +72,3 @@ def postprocess(results, output_name, batch_size, batching):
             output += "    {} ({}) = {}".format(cls[0], cls[1], cls[2])
 
     return output
-
-trition_client = None
-_url = "localhost:8000"
-_model = "densenet_onnx"
-_scaling = "INCEPTION"
-
-def init():
-    global triton_client, max_batch_size, input_name, output_name, dtype
-    
-    print("Connect to Triton")
-    triton_client = tritonhttpclient.InferenceServerClient(_url)
-    
-    max_batch_size = 0
-    input_name = "data_0"
-    output_name = "fc6_1"
-    dtype = "FP32"
-
-@rawhttp
-def run(request):
-    if request.method == 'POST':
-        
-        reqBody = request.get_data(False)
-        img = Image.open(io.BytesIO(reqBody))
-        
-        image_data = preprocess(img, _scaling)
-        
-        input = tritonhttpclient.InferInput(input_name, image_data.shape, dtype)
-        input.set_data_from_numpy(image_data, binary_data=True)
-        output = tritonhttpclient.InferRequestedOutput(output_name, binary_data=True, class_count=1)
-    
-        res = triton_client.infer(_model,
-                                [input],
-                                request_id="0",
-                                outputs=[output])
-
-        result = postprocess(res, output_name, 1, max_batch_size > 0)
-
-        return AMLResponse(result, 200)
-    else:
-        return AMLResponse("bad request", 500)
