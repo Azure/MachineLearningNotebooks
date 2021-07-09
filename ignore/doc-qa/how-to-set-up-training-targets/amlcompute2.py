6a145086d8351e572c30a582a4fefdf96a5b23dc
Larry Franks <larry.franks@microsoft.com>
2021-07-09 10:37:21 -0400

    Note about setting location.
    @sdgilley Is this update OK? We want to show that location can be set for a new public preview functionality.

--- ignore/doc-qa/how-to-set-up-training-targets/amlcompute2.py
+++ ignore/doc-qa/how-to-set-up-training-targets/amlcompute2.py
@@ -28,6 +28,7 @@ try:
     cpu_cluster = ComputeTarget(workspace=ws, name=cpu_cluster_name)
     print('Found existing cluster, use it.')
 except ComputeTargetException:
+    # To use a different region for the compute, add a location='<region>' parameter
     compute_config = AmlCompute.provisioning_configuration(vm_size='STANDARD_D2_V2',
                                                            max_nodes=4)
     cpu_cluster = ComputeTarget.create(ws, cpu_cluster_name, compute_config)
