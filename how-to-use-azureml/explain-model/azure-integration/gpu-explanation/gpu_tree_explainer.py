8d3f5adcdb5518950d73fca0351ce1f74b9eb88f
amlrelsa-ms <amlrelsa@microsoft.com>
2022-06-27 17:29:38 +0000

    update samples from Release-147 as a part of  SDK release

--- how-to-use-azureml/explain-model/azure-integration/gpu-explanation/gpu_tree_explainer.py
+++ how-to-use-azureml/explain-model/azure-integration/gpu-explanation/gpu_tree_explainer.py
@@ -2,6 +2,8 @@
 # Licensed under the MIT license.
 
 from azureml.core.run import Run
+from azureml.interpret import ExplanationClient
+from interpret_community.adapter import ExplanationAdapter
 import joblib
 import os
 import shap
@@ -11,9 +13,11 @@ OUTPUT_DIR = './outputs/'
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 
 run = Run.get_context()
+client = ExplanationClient.from_run(run)
 
 # get a dataset on income prediction
 X, y = shap.datasets.adult()
+features = X.columns.values
 
 # train an XGBoost model (but any other tree model type should work)
 model = xgboost.XGBClassifier()
@@ -25,6 +29,12 @@ shap_values = explainer(X_shap)
 
 print("computed shap values:")
 print(shap_values)
+
+# Use the explanation adapter to convert the importances into an interpret-community
+# style explanation which can be uploaded to AzureML or visualized in the
+# ExplanationDashboard widget
+adapter = ExplanationAdapter(features, classification=True)
+global_explanation = adapter.create_global(shap_values.values, X_shap, expected_values=shap_values.base_values)
 
 # write X_shap out as a pickle file for later visualization
 x_shap_pkl = 'x_shap.pkl'
@@ -42,3 +52,8 @@ with open(model_file_name, 'wb') as file:
 run.upload_file('xgboost_model.pkl', os.path.join('./outputs/', model_file_name))
 original_model = run.register_model(model_name='xgboost_with_gpu_tree_explainer',
                                     model_path='xgboost_model.pkl')
+
+# Uploading model explanation data for storage or visualization in webUX
+# The explanation can then be downloaded on any compute
+comment = 'Global explanation on classification model trained on adult census income dataset'
+client.upload_model_explanation(global_explanation, comment=comment, model_id=original_model.id)
--- how-to-use-azureml/explain-model/azure-integration/scoring-time/score_local_explain.py
+++ how-to-use-azureml/explain-model/azure-integration/scoring-time/score_local_explain.py
@@ -18,7 +18,9 @@ def init():
     original_model_path = Model.get_model_path('local_deploy_model')
     scoring_explainer_path = Model.get_model_path('IBM_attrition_explainer')
 
+    # Load the original model into the environment
     original_model = joblib.load(original_model_path)
+    # Load the scoring explainer into the environment
     scoring_explainer = joblib.load(scoring_explainer_path)
 
 
@@ -29,5 +31,15 @@ def run(raw_data):
     predictions = original_model.predict(data)
     # Retrieve model explanations
     local_importance_values = scoring_explainer.explain(data)
+    # Retrieve the feature names, which we may want to return to the user.
+    # Note: you can also get the raw_features and engineered_features
+    # by calling scoring_explainer.raw_features and
+    # scoring_explainer.engineered_features but you may need to pass
+    # the raw or engineered feature names in the ScoringExplainer
+    # constructor, depending on if you are using feature maps or
+    # transformations on the original explainer.
+    features = scoring_explainer.features
     # You can return any data type as long as it is JSON-serializable
-    return {'predictions': predictions.tolist(), 'local_importance_values': local_importance_values}
+    return {'predictions': predictions.tolist(),
+            'local_importance_values': local_importance_values,
+            'features': features}
