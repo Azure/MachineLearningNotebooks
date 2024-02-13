f0d7379af704f3b8c0a8945c1083f3c1f55c8db5
Jeff Shepherd <jeffshep@microsoft.com>
2024-02-13 11:47:56 -0800

    Updated curated environments in sample notebooks

--- how-to-use-azureml/ml-frameworks/pytorch/distributed-pytorch-with-horovod/pytorch_horovod_mnist.py
+++ how-to-use-azureml/ml-frameworks/pytorch/distributed-pytorch-with-horovod/pytorch_horovod_mnist.py
@@ -51,15 +51,15 @@ if args.cuda:
 
 
 kwargs = {}
-# Use Azure Open Datasets for MNIST dataset
+# MNIST dataset
 datasets.MNIST.resources = [
-    ("https://azureopendatastorage.azurefd.net/mnist/train-images-idx3-ubyte.gz",
+    ("train-images-idx3-ubyte.gz",
      "f68b3c2dcbeaaa9fbdd348bbdeb94873"),
-    ("https://azureopendatastorage.azurefd.net/mnist/train-labels-idx1-ubyte.gz",
+    ("train-labels-idx1-ubyte.gz",
      "d53e105ee54ea40749a09fcbcd1e9432"),
-    ("https://azureopendatastorage.azurefd.net/mnist/t10k-images-idx3-ubyte.gz",
+    ("t10k-images-idx3-ubyte.gz",
      "9fb629c4189551a2d022fa330f9573f3"),
-    ("https://azureopendatastorage.azurefd.net/mnist/t10k-labels-idx1-ubyte.gz",
+    ("t10k-labels-idx1-ubyte.gz",
      "ec29112dd5afa0611ce80d1b7f02629c")
 ]
 train_dataset = \
